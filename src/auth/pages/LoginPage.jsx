05748aa31b274122fdd768aa78521448d341a702
Majo <mariajose72003@gmail.com>
2024-11-02 17:09:27 -0500

    login and register desig

--- src/auth/pages/LoginPage.jsx
+++ src/auth/pages/LoginPage.jsx
@@ -1,8 +1,6 @@
 import { useContext } from "react";
 import { useNavigate, Link, NavLink } from "react-router-dom";
-
 import { AuthContext } from "../context";
-import { Navbar } from "../../ui/components/common/Navbar";
 import { useForm } from "../../hooks";
 
 const initForm = {
@@ -12,100 +10,81 @@ const initForm = {
 
 export const LoginPage = () => {
   const { login, errorMessage, loginGoogle } = useContext(AuthContext);
-
   const navigate = useNavigate();
-
   const { email, password, onInputChange } = useForm(initForm);
 
   const onLogin = async (event) => {
     event.preventDefault();
     const isValidLogin = await login(email, password);
-
     if (isValidLogin) {
       const lastPath = localStorage.getItem("lastPath") || "/";
-      navigate(lastPath, {
-        replace: true,
-      });
+      navigate(lastPath, { replace: true });
     }
   };
 
   const onGoogleLogin = async (event) => {
     event.preventDefault();
     const isValidLogin = await loginGoogle();
-
     if (isValidLogin) {
       const lastPath = localStorage.getItem("lastPath") || "/";
-      navigate(lastPath, {
-        replace: true,
-      });
+      navigate(lastPath, { replace: true });
     }
   };
 
   return (
-    <>
-      <div className="min-h-screen flex items-center justify-center bg-gray-100">
-        <div className="max-w-md w-full bg-white p-8 shadow-lg rounded-lg">
-          <h2 className="text-2xl font-bold mb-4">Login</h2>
-          <form>
-            <div className="mb-4">
-              <label
-                htmlFor="email"
-                className="block text-sm font-medium text-gray-700"
-              >
-                Email
-              </label>
-              <input
-                type="email"
-                id="email"
-                name="email"
-                value={email}
-                onChange={onInputChange}
-                placeholder="Enter email"
-                className="mt-1 p-2 w-full border rounded-md"
-              />
+    <div className="min-h-screen flex items-center justify-center bg-blue-50">
+      <div className="max-w-md w-full bg-white p-8 shadow-xl rounded-xl">
+        <h2 className="text-3xl font-bold text-blue-500 mb-6">Login</h2>
+        <form>
+          <div className="mb-4">
+            <label htmlFor="email" className="block text-sm font-medium text-gray-700">
+              Username
+            </label>
+            <input
+              type="email"
+              id="email"
+              name="email"
+              value={email}
+              onChange={onInputChange}
+              placeholder="Enter email"
+              className="mt-1 p-2 w-full border rounded-md"
+            />
+          </div>
+          <div className="mb-6">
+            <label htmlFor="password" className="block text-sm font-medium text-gray-700">
+              Password
+            </label>
+            <input
+              type="password"
+              id="password"
+              name="password"
+              value={password}
+              onChange={onInputChange}
+              placeholder="Enter password"
+              className="mt-1 p-2 w-full border rounded-md"
+            />
+          </div>
+          <button
+            onClick={onLogin}
+            type="submit"
+            className="w-full bg-blue-500 text-white py-2 rounded-md hover:bg-blue-600 transition duration-200"
+          >
+            Login
+          </button>
+          {errorMessage && (
+            <div className="mt-4 text-red-500 text-sm" role="alert">
+              {errorMessage}
             </div>
-            <div className="mb-6">
-              <label
-                htmlFor="password"
-                className="block text-sm font-medium text-gray-700"
-              >
-                password
-              </label>
-              <input
-                type="password"
-                id="password"
-                name="password"
-                value={password}
-                onChange={onInputChange}
-                placeholder="Enter password"
-                className="mt-1 p-2 w-full border rounded-md"
-              />
-            </div>
-            <button
-              onClick={onLogin}
-              type="submit"
-              className="w-full bg-teal-600 text-white py-2 rounded-md hover:bg-teal-700"
-            >
-              Login
-            </button>
-            <button
-              onClick={onGoogleLogin}
-              type="submit"
-              className="w-full bg-teal-400 text-white py-2 rounded-md hover:bg-teal-700"
-            >
-              Google
-            </button>
-            <br />
-            {!errorMessage ? null : (
-              <div className="alert alert-danger" role="alert">
-                {errorMessage}
-              </div>
-            )}
-          </form>
-          <span>Do you want create a new account?</span>
-          <Link to="/register"> Register</Link>
+          )}
+        </form>
+        
+        <div className="mt-4 text-sm text-gray-500">
+          <span>Don't have an account?</span>
+          <NavLink to="/register" className="text-blue-500 hover:underline">
+            {" "}Register
+          </NavLink>
         </div>
       </div>
-    </>
+    </div>
   );
 };
--- src/auth/pages/RegisterPage.jsx
+++ src/auth/pages/RegisterPage.jsx
@@ -8,16 +8,14 @@ const initForm = {
   password: "",
   displayName: "",
 };
+
 export const RegisterPage = () => {
   const { register, errorMessage } = useContext(AuthContext);
-
   const navigate = useNavigate();
-
   const { email, password, displayName, onInputChange } = useForm(initForm);
 
   const onRegister = async (event) => {
     event.preventDefault();
-
     const isValidRegister = await register(email, password, displayName);
 
     if (isValidRegister) {
@@ -28,16 +26,16 @@ export const RegisterPage = () => {
 
   return (
     <>
-      <div className="min-h-screen flex items-center justify-center bg-gray-100">
+      <div className="min-h-screen flex items-center justify-center bg-blue-50">
         <div className="max-w-md w-full bg-white p-8 shadow-lg rounded-lg">
-          <h2 className="text-2xl font-bold mb-4">Register</h2>
+          <h2 className="text-2xl font-bold mb-4 text-blue-700">Register</h2>
           <form>
             <div className="mb-4">
               <label
-                htmlFor="email"
+                htmlFor="displayName"
                 className="block text-sm font-medium text-gray-700"
               >
-                User name
+                Name
               </label>
               <input
                 type="text"
@@ -45,7 +43,41 @@ export const RegisterPage = () => {
                 name="displayName"
                 value={displayName}
                 onChange={onInputChange}
-                placeholder="Enter user name"
+                placeholder="Enter name"
+                className="mt-1 p-2 w-full border rounded-md"
+              />
+            </div>
+            <div className="mb-4">
+              <label
+                htmlFor="email"
+                className="block text-sm font-medium text-gray-700"
+              >
+                Last Name
+              </label>
+              <input
+                type="text"
+                id="lastName"
+                name="lastName"
+                value={displayName}
+                onChange={onInputChange}
+                placeholder="Enter last name"
+                className="mt-1 p-2 w-full border rounded-md"
+              />
+            </div>
+            <div className="mb-4">
+              <label
+                htmlFor="username"
+                className="block text-sm font-medium text-gray-700"
+              >
+                Username
+              </label>
+              <input
+                type="text"
+                id="username"
+                name="username"
+                value={displayName}
+                onChange={onInputChange}
+                placeholder="Enter username"
                 className="mt-1 p-2 w-full border rounded-md"
               />
             </div>
@@ -71,7 +103,7 @@ export const RegisterPage = () => {
                 htmlFor="password"
                 className="block text-sm font-medium text-gray-700"
               >
-                password
+                Password
               </label>
               <input
                 type="password"
@@ -86,19 +118,21 @@ export const RegisterPage = () => {
             <button
               onClick={onRegister}
               type="submit"
-              className="w-full bg-teal-600 text-white py-2 rounded-md hover:bg-teal-700"
+              className="w-full bg-blue-600 text-white py-2 rounded-md hover:bg-blue-700"
             >
               Register
             </button>
             <br />
             {!errorMessage ? null : (
-              <div className="alert alert-danger" role="alert">
+              <div className="alert alert-danger mt-2 text-red-600" role="alert">
                 {errorMessage}
               </div>
             )}
           </form>
-          <span>Do you have an existing account?</span>
-          <Link to="/login"> Login</Link>
+          <span className="text-gray-700">Do you have an existing account?</span>
+          <Link to="/login" className="text-blue-500 ml-1">
+            Login
+          </Link>
         </div>
       </div>
     </>
